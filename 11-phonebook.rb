bcf5962793e0db1da20eb7b2890569a1ac2625c8
mfirmanakbar <em.firman.akbar@gmail.com>
2019-07-13 10:54:25 +0700

    temporary
    correct

--- 11-phonebook.rb
+++ 11-phonebook.rb
@@ -1,43 +1,20 @@
 def findPhoneBook(arrData, arrFind)
 
   # puts arrData[0][0].include? ('sam')
-
   # puts 2 > 3 ? "truex" : "falsex"
   # puts (arrData.select { |n| n[0] == 'sam' }) != nil ? true : false
 
-  # res = arrData.select{ |n| n[0] == 'sam' }
-  # if res.length > 0
-  #   puts res.join(' ')
-  # else
-  #   puts 'Not found'
-  # end
-
   arrFind.each_with_index do |val,i|
-    cx = val
-    res = arrData.select{ |n| n[0].eql? cx }
+    res = arrData.select{ |n| n[0].eql? arrFind[i][0] }
     if res.length > 0
-      puts res.join(' ')
+      puts res.join('=')
     else
       puts 'Not found'
     end
-    # puts arrFind[i]
-    # puts arrData.select{ |value| value[0].include? arrFind[i] }
-    # puts arrData.select{ |value| value.include? 'sam' }
-    # [ [user1,true], [user2,true], [user3,false] ].detect { |user, boolean| user.id == 2 }
-    # puts arrData.detect { |str1, str2| str1 == arrFind[i] }
   end
-
 
 end
 
-# {condition} ? {if-code-block} : {else-code-block}
-
-# cx = 'ss'
-# if cx.to_s.empty?
-#   puts "Yes Empty"
-# else
-#   puts "Not Empty"
-# end
 
 n = gets.to_i
 
